e16fdd304a061ecb362f92ee7081e850fd3ad677
KR企画 <krkikaku13@gmail.com>
2026-01-11 15:04:56 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,18 +9,27 @@ import CompanySection from '@/components/public/CompanySection';
 import ContactSection from '@/components/public/ContactSection';
 
 export default function Home() {
+  const handleNavigate = (id) => {
+    const element = document.getElementById(id);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
   return (
     <div className="min-h-screen bg-white">
-      <Header />
+      <Header onNavigate={handleNavigate} />
+      <div className="mt-8 md:mt-12"></div>
       <main>
         <Carousel />
+        <div className="-mt-12 md:-mt-16"></div>
         <GreetingSection />
         <BusinessSection />
         <NewsSection />
         <CompanySection />
         <ContactSection />
       </main>
-      <Footer />
+      <Footer onNavigate={handleNavigate} />
     </div>
   );
 }
