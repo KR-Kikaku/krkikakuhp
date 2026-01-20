281d001b4c2c4fbfbef7171a5d27519dc4d71ba4
KR企画 <krkikaku13@gmail.com>
2026-01-20 04:26:20 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,21 +1,27 @@
 import React, { useEffect } from 'react';
-import { Helmet } from 'react-helmet';
 
 export default function Layout({ children }) {
   useEffect(() => {
     // HTMLのlang属性を設定
     document.documentElement.lang = 'ja';
+    
+    // metaタグを設定
+    let metaGoogle = document.querySelector('meta[name="google"]');
+    if (!metaGoogle) {
+      metaGoogle = document.createElement('meta');
+      metaGoogle.name = 'google';
+      metaGoogle.content = 'notranslate';
+      document.head.appendChild(metaGoogle);
+    }
+    
+    let metaContentLanguage = document.querySelector('meta[http-equiv="Content-Language"]');
+    if (!metaContentLanguage) {
+      metaContentLanguage = document.createElement('meta');
+      metaContentLanguage.httpEquiv = 'Content-Language';
+      metaContentLanguage.content = 'ja';
+      document.head.appendChild(metaContentLanguage);
+    }
   }, []);
 
-  return (
-    <>
-      <Helmet>
-        <html lang="ja" />
-        <meta name="google" content="notranslate" />
-        <meta httpEquiv="Content-Language" content="ja" />
-        <title>KR企画</title>
-      </Helmet>
-      {children}
-    </>
-  );
+  return <>{children}</>;
 }
